3f7cd065a53e2fd213da449c1ca76bd7b1e51a5b
Robert Niemela <rvniemela@hotmail.com>
2022-04-12 15:11:50 -0700

    refactor: make nav menu responsive

---
+++ script.js
@@ -0,0 +1,21 @@
+const navIcon = document.getElementById('nav-checkbox')
+const nav = document.getElementById('nav')
+
+navIcon.addEventListener('click', () => {
+  console.log(nav)
+  nav.classList.toggle('nav-open')
+})
+const navLinks = document.getElementsByClassName('nav-item-link')
+
+console.log(navLinks)
+
+for (let i = 0; i < navLinks.length; i++) {
+  console.log(link)
+  link.addEventListener('click', () => {
+    closeNav()
+  })
+}
+
+const closeNav = () => {
+  nav.classList.toggle('nav-open')
+}
