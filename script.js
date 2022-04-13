97369283a51a579bd172f5aedddabb9329ba06f0
Robert Niemela <rvniemela@hotmail.com>
2022-04-13 11:40:12 -0700

    refactor: remove ability to scroll when nav is open

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Handle opening/closing nav menu
 const navIcon = document.getElementById('nav-checkbox')
-const nav = document.getElementById('nav')
+const body = document.getElementById('body')
 
 navIcon.addEventListener('click', () => toggleNav())
 
 const toggleNav = (navState) => {
   if (navState === true) {
-    nav.classList.add('nav-open')
+    body.classList.add('nav-open')
   } else if (navState === false) {
-    nav.classList.remove('nav-open')
-  } else nav.classList.toggle('nav-open')
+    body.classList.remove('nav-open')
+  } else body.classList.toggle('nav-open')
 }
 
 // Make clicking nav links close nav menu
