1577fe86b0de6b2f7dd5bf252f078d8c1d1c57aa
Robert Niemela <rvniemela@hotmail.com>
2022-04-12 19:08:17 -0700

    refactor: use one function for opening/closing nav

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
+// Handle opening/closing nav menu
 const navIcon = document.getElementById('nav-checkbox')
 const nav = document.getElementById('nav')
 
-navIcon.addEventListener('click', () => {
-  console.log(nav)
-  nav.classList.toggle('nav-open')
-})
-const navLinks = document.getElementsByClassName('nav-item-link')
+navIcon.addEventListener('click', () => toggleNav())
 
-console.log(navLinks)
-
-for (let i = 0; i < navLinks.length; i++) {
-  console.log(link)
-  link.addEventListener('click', () => {
-    closeNav()
-  })
+const toggleNav = (navState) => {
+  if (navState === true) {
+    nav.classList.add('nav-open')
+  } else if (navState === false) {
+    nav.classList.remove('nav-open')
+  } else nav.classList.toggle('nav-open')
 }
 
-const closeNav = () => {
-  nav.classList.remove('nav-open')
+// Make clicking nav links close nav menu
+const navLinks = document.getElementsByClassName('nav-item-link')
+for (let i = 0; i < navLinks.length; i++) {
+  console.log(navLinks[i])
+  navLinks[i].addEventListener('click', () => {
+    toggleNav(false)
+  })
 }
